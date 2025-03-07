dcd26fe301218d2acf6b6046f148437f735d9abb
gloriousnetworker <iniubongudofot2000@gmail.com>
2025-03-07 17:05:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Responsiveness for navbar 
+function uncheckCheckbox(){
+    document.getElementById("check").checked = false;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function() {
+        document.querySelector(".fadeInOnLoad").style.opacity = "1";
+    }, 100);
+});
