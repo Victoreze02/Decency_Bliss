888ce36dc760dc90f279c8bd1e8a372e976caf0e
gloriousnetworker <iniubongudofot2000@gmail.com>
2025-03-08 03:50:39 +0100

    second commit

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".fadeInOnLoad").style.opacity = "1";
     }, 100);
 });
+
+
+
+ document.getElementById("emailForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    var formData = new FormData(this);
+
+    fetch("send_email.php", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("response").innerText = data;
+    })
+    .catch(error => console.error("Error:", error));
+});
